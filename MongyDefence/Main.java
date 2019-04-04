950977179f37ca359ee2d657c7122d1359a71124
ItFalls <46503654+ItFalls@users.noreply.github.com>
2019-04-03 21:02:23 -0700

    resetored??

--- MongyDefence/Main.java
+++ MongyDefence/Main.java
@@ -1,4 +1,38 @@
 
-public class Main {
+import java.awt.*;
+import javax.swing.*;
 
+public class Main extends JPanel {
+	private final Font FONT = new Font("ComicSans", Font.PLAIN, 20);
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.setColor(Color.GREEN);
+		
+		g.fillRect(0, 0, getWidth(), 5*getHeight()/8);
+		
+		g.setColor(Color.BLACK);
+		
+		g.setFont(FONT);
+		
+		g.drawString("Power Ups", getWidth()/2 - 30, 11*getHeight()/16);
+		
+		g.drawLine(0, 23*getHeight()/32, getWidth(), 23*getHeight()/32);
+	}
+	
+	public static void main(String[] args) {
+		JFrame window = new JFrame("MongyDefense");
+		
+		window.setBounds(300,300,800,600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Main panel = new Main();
+		panel.setBackground(Color.PINK);
+		Container c = window.getContentPane();
+		c.add(panel);
+		
+		window.setVisible(true);
+	}
+	
 }
